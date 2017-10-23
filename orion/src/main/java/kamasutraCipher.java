898efbd4d41f9367d9f85e12c58518ecd57b4b5a
CyclingNinja <bennett.sm89@gmail.com>
2017-10-23 17:23:17 +0100

    Add kamasutraCipher.java

---
+++ orion/src/main/java/kamasutraCipher.java
@@ -0,0 +1,20 @@
+import java.util.TreeMap
+
+public class kamasutraCipher {
+
+    public static String main(String[] args) {
+        String message = new String(args[0]);
+        String key = new String(args[1]);
+
+        // instantiate the lookup treemap
+        TreeMap <String, String> cipherDict =
+            new TreeMap<String, String>();
+
+        // check that the two strings are the same length
+        if (message.length() == key.length());
+            for (int i = 0; i < message.length(); i++);
+
+
+    }
+
+}
